3e540c532470f91be00e70203918c6d1730feb2d
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-04-02 00:17:27 +0700

    feat: add restaurant details page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Navbar } from './component/Navbar/Navbar';
 import { darkTheme } from './theme/DarkTheme';
 import Home from './component/Navbar/Home/Home';
+import RestaurantDetails from './component/Restaurant/RestaurantDetails';
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      <RestaurantDetails />
     </ThemeProvider>
   );
 }
---
+++ src/component/Restaurant/MenuCard.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+
+
+const demo = [
+    {
+        category: "Nuts & seeds",
+        ingredients: ["Cashews"]
+    },
+    {
+        category: "Protein",
+        ingredients: ["Protein", "Bacon strips"]
+    },
+]
+
+const MenuCard = () => {
+    const handleCheckBoxChange = (value) => {
+        console.log("value")
+    }
+    return (
+        <Accordion>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+            >
+                <div className='lg:flex justify-between items-center'>
+                    <div className='lg:flex items-center lg:gap-5'>
+                        <img
+                            className='w-[7rem] h-[7rem] object-cover'
+                            src="https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt=""
+                        />
+                    </div>
+                    <div className='space-y-1 lg:space-y-5 lg:max-w-2xl lg:pl-5'>
+                        <p className='text-xl font-semibold'>Burger</p>
+                        <p>$499</p>
+                        <p className='text-gray-400'>nice food</p>
+                    </div>
+                </div>
+            </AccordionSummary>
+            <AccordionDetails>
+                <form>
+                    <div className='flex gap-5 flex-wrap'>
+                        {
+                            demo.map((item) =>
+                                <div>
+                                    <p>{item.category}</p>
+                                    <FormGroup>
+                                        {item.ingredients.map((item) => <FormControlLabel
+                                            control={<Checkbox onChange={() => handleCheckBoxChange(item)} />} label={item} />)}                                   <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+                                    </FormGroup>
+                                </div>
+                            )
+                        }
+                    </div>
+                    <div className='pt-5'>
+                        <Button variant='contained' disabled={false} type='submit'>{true ? 'Add to Cart' : 'Out of Stock'}</Button>
+                    </div>
+                </form>
+            </AccordionDetails>
+        </Accordion>
+    )
+}
+
+export default MenuCard
