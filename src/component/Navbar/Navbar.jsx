535bdb755e3c94bf8c759f4142d9fba0dcd3db9e
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-04-02 23:23:27 +0700

    feat: add auth form

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -4,9 +4,12 @@ import { pink } from '@mui/material/colors';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import "./Navbar.css"
+import { Person } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <div className='px-5 sticky top-0 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between'>
       <div className='flex items-center space-x-4'>
@@ -23,7 +26,10 @@ export const Navbar = () => {
           </IconButton>
         </div>
         <div className='flex items-center space'>
-          <Avatar sx={{ backgroundColor: '#ffffff', color: pink.A400 }}>C</Avatar>
+          {false ? <Avatar sx={{ backgroundColor: '#ffffff', color: pink.A400 }}>C</Avatar> :
+            <IconButton onClick={() => navigate('/account/login')}>
+              <Person />
+            </IconButton>}
         </div>
         <div className=''>
           <IconButton >
