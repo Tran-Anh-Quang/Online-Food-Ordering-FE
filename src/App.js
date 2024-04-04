4e4dd5431f6bb4363920ad48f13b7a04dcf29e64
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-04-04 18:34:45 +0700

    feat: fetch restaurant api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import './App.css';
-import { Navbar } from './component/Navbar/Navbar';
 import { darkTheme } from './theme/DarkTheme';
-import Home from './component/Home/Home';
-import RestaurantDetails from './component/Restaurant/RestaurantDetails';
-import Cart from './component/Cart/Cart';
-import Profile from './component/Profile/Profile';
 import { CustomRoute } from './Routers/CustomRoute';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
