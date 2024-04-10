a5cf0dd50fe8e3d15322d756de4cd30fb287c932
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-04-10 21:27:36 +0700

    chore: update profile screen

--- src/component/Profile/UserProfile.jsx
+++ src/component/Profile/UserProfile.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Button } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../State/Authenticate/Action';
+import { useNavigate } from 'react-router-dom';
 
 const UserProfile = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { auth } = useSelector((store) => store)
     const handleLogout = () => {
-
+            dispatch(logout());
+            navigate("/")
     }
     return (
         <div className='min-h-[80vh] flex flex-col justify-center items-center text-center'>
             <div className='flex flex-col justify-center items-center text-center'>
                 <AccountCircleIcon sx={{ fontSize: '9rem' }} />
-                <h1 className='py-5 text-2xl font-semibold'>Tran Anh Quang</h1>
-                <p>Email: dinosaurous9x@gmail.com</p>
+                <h1 className='py-5 text-2xl font-semibold'>{auth.user?.fullName.toUpperCase()}</h1>
+                <p>Email: {auth.user?.email}</p>
                 <Button variant='contained' onClick={handleLogout} sx={{margin: '2rem 0rem'}}>Logout</Button>
             </div>
         </div>
